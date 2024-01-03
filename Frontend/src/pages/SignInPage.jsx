ef9647d9b14b784ec8b89ff0a52dcfc2be7d4355
JonSEK <engkien@ymail.com>
2024-01-03 23:27:07 +0800

    Add sign-in functionality to SignInPage component

--- Frontend/src/pages/SignInPage.jsx
+++ Frontend/src/pages/SignInPage.jsx
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "../signUpPage.css";
 
-export default function SignInPage() {
-  return <div>SignInPage</div>;
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/auth/signin", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.data;
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.error);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.response?.data?.message || error.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading} type="submit">
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div>
+        <p>Don&apos;t have an account?</p>
+        <Link to={"/sign-up"}>
+          <span>Sign Up</span>
+        </Link>
+      </div>
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
