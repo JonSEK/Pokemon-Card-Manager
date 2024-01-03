0d77862a5909b7dc433f794d9bcf9e2e76efd96e
JonSEK <engkien@ymail.com>
2024-01-03 22:59:58 +0800

    Add signin functionality

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,13 +1,19 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password, confirmPassword } = req.body;
   if (!username || !email || !password || !confirmPassword) {
-    return res.status(400).json({ success: false, error: "Please fill in all fields" });
+    return res
+      .status(400)
+      .json({ success: false, error: "Please fill in all fields" });
   }
   if (password !== confirmPassword) {
-    return res.status(400).json({ success: false, error: "Passwords do not match" });
+    return res
+      .status(400)
+      .json({ success: false, error: "Passwords do not match" });
   }
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -19,6 +25,33 @@ export const signup = async (req, res, next) => {
   try {
     await user.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Please fill in all fields" });
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...others } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(others);
   } catch (error) {
     next(error);
   }
